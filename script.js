2c56c46e1fd540a5d4c30f19965fe2aeeff45f42
MrSautchuk <allasautchuk@gmail.com>
2024-11-26 21:32:35 -0300

    Nova alteração

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ let currentIndex = 0;
 
 document.querySelector('.prev').addEventListener('click', () => {
     const carousel = document.querySelector('.carousel-container');
-    currentIndex = (currentIndex > 0) ? currentIndex - 1 : document.querySelectorAll('.service').length - 1;
-    carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+    const totalItems = document.querySelectorAll('.service').length;
+    currentIndex = (currentIndex > 0) ? currentIndex - 1 : totalItems - 1;
+    carousel.style.transform = `translateX(-${currentIndex * 104}%)`;
 });
 
 document.querySelector('.next').addEventListener('click', () => {
     const carousel = document.querySelector('.carousel-container');
-    currentIndex = (currentIndex < document.querySelectorAll('.service').length - 1) ? currentIndex + 1 : 0;
-    carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+    const totalItems = document.querySelectorAll('.service').length;
+    currentIndex = (currentIndex < totalItems - 1) ? currentIndex + 1 : 0;
+    carousel.style.transform = `translateX(-${currentIndex * 104}%)`;
 });
