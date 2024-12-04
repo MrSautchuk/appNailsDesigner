94ca36cd1599e728e09c27a47dde0f8ab38c2e2a
MrSautchuk <allasautchuk@gmail.com>
2024-12-03 21:28:55 -0300

    js

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ document.querySelector('.next').addEventListener('click', () => {
     currentIndex = (currentIndex < totalItems - 1) ? currentIndex + 1 : 0;
     carousel.style.transform = `translateX(-${currentIndex * 104}%)`;
 });
+
+
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('showing');
+});
